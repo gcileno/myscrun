a0e8cd56134647176714525f065a3d4ba5b6961b
gcmc <gcmc@softex.cin.ufpe.br>
2026-03-10 14:59:21 -0300

    ajuste no admin do app Member

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Member, Organization
+from .models import Member, Organization, OrganizationMember
 
 
 @admin.register(Member)
@@ -7,8 +7,13 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "user")
     search_fields = ("name", "user__username")
 
-
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "cnpj", "director", "created_at")
     search_fields = ("name", "cnpj")
+
+
+@admin.register(OrganizationMember)
+class OrganizationMemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "member", "invited", "accepted", "is_active")
+    search_fields = ("organization__name", "member__name")
