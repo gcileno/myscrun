b312150b711cc942bfa24a45a34381afadc181cc
gcmc <gcmc@softex.cin.ufpe.br>
2026-03-11 15:02:55 -0300

    correçao de erro

--- scrun_master/serializers/team.py
+++ scrun_master/serializers/team.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from scrun_master.models import TeamMember
-from .projects import ProjectSimpleSerializer
 
 class TeamMemberSerializer(serializers.ModelSerializer):
-    project = ProjectSimpleSerializer(read_only=True)
 
     class Meta:
         model = TeamMember
