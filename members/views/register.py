f9bb32312e7b7045ead4d188b77cc9ad1163b81c
gcmc <gcmc@softex.cin.ufpe.br>
2026-04-23 11:37:51 -0300

    Serializer para registro de novos usuarios

---
+++ members/views/register.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from members.serializers.user import RegisterSerializer
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'token': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
